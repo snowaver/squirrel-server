aa90b2b1fe6ef7645c851a26748a40a62bb5f4ca
snowaver <1123009419@qq.com>
2019-06-08 14:27:07 +0800

    1/resolve wrong packet type which is sent to the subscriber when accept a subscribe.

--- src/main/java/cc/mashroom/squirrel/module/user/controller/ContactController.java
+++ src/main/java/cc/mashroom/squirrel/module/user/controller/ContactController.java
@@ -25,6 +25,7 @@ import  org.springframework.web.bind.annotation.RestController;
 
 import  cc.mashroom.squirrel.common.AbstractController;
 import  cc.mashroom.squirrel.module.user.service.ContactService;
+import  cc.mashroom.squirrel.paip.message.subscribes.SubscribeAckPacket;
 import  cc.mashroom.squirrel.paip.message.subscribes.SubscribePacket;
 import  cc.mashroom.squirrel.paip.message.subscribes.UnsubscribePacket;
 import  cc.mashroom.squirrel.server.handler.PacketRoute;
@@ -43,7 +44,7 @@ public  class  ContactController  extends  AbstractController
 	{
 		ResponseEntity<Map<String,Object>>  responseEntity = service.subscribe( sessionProfile.getLong("USER_ID"),subscribeeId,remark,group );
 		
-		PacketRoute.INSTANCE.route( subscribeeId,new  SubscribePacket(sessionProfile.getLong("USER_ID"),(Map<String,Object>)  responseEntity.getBody().remove("SUBSCRIBER_PROFILE")) );
+		PacketRoute.INSTANCE.route( subscribeeId,new  SubscribePacket(sessionProfile.getLong("USER_ID")     ,(Map<String,Object>)  responseEntity.getBody().remove("SUBSCRIBER_PROFILE")) );
 		
 		return  ResponseEntity.status(200).body( JsonUtils.toJson( responseEntity.getBody()  ) );
 	}
@@ -55,7 +56,7 @@ public  class  ContactController  extends  AbstractController
 	{
 		ResponseEntity<Map<String,Object>>  responseEntity = service.changeSubscribeStatus( status,subscriberId,sessionProfile.getLong("USER_ID"),remark,group );
 		
-		PacketRoute.INSTANCE.route( subscriberId,new  SubscribePacket(sessionProfile.getLong("USER_ID"),(Map<String,Object>)  responseEntity.getBody().remove("SUBSCRIBEE_PROFILE")) );
+		PacketRoute.INSTANCE.route( subscriberId,new  SubscribeAckPacket(sessionProfile.getLong("USER_ID"),7,(Map<String,Object>)  responseEntity.getBody().remove("SUBSCRIBEE_PROFILE")) );
 		
 		return  ResponseEntity.status(200).body( JsonUtils.toJson( responseEntity.getBody()  ) );
 	}
